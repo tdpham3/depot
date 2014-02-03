5d7d440db114806551a1b612e6c9dd88a892832a
Tri D. Pham <tdpham3@gmail.com>
2014-02-03 12:58:39 -0800

    Finish Ch. 11

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -6,11 +6,20 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-module ApplicationHelper
-  def hidden_div_if(condition, attributes = {}, &block)
-    if condition
-      attributes["style"] = "display: none"
+class Cart < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
     end
-    content_tag("div", attributes, &block)
+    current_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
   end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -6,11 +6,11 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
-module ApplicationHelper
-  def hidden_div_if(condition, attributes = {}, &block)
-    if condition
-      attributes["style"] = "display: none"
-    end
-    content_tag("div", attributes, &block)
+class LineItem < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :cart
+
+  def total_price
+    product.price * quantity
   end
 end
